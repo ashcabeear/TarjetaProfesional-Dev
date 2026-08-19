697528a6f203eefc03b32f1ce435171e3ce73df0
Ash Bean <ashleybean2603@gmail.com>
2026-08-18 21:11:09 -0500

    Prueba wpp 2

--- PresentacionProfesional/src/AbrirWhatsapp.ts
+++ PresentacionProfesional/src/AbrirWhatsapp.ts
@@ -3,15 +3,18 @@ import * as ecs from '@8thwall/ecs'
 ecs.registerComponent({
   name: 'Abrir WhatsApp',
 
-  stateMachine: ({eid}) => {
-    ecs.defineState('default')
-      .initial()
-      .listen(eid, ecs.input.UI_CLICK, () => {
+  add: (world, component) => {
+    world.events.addListener(
+      component.eid,
+      ecs.input.SCREEN_TOUCH_START,
+      () => {
+        console.log('WhatsApp clicked!')
 
-        console.log('WHATSAPP CLICK')
-
-        window.location.href = 'https://wa.me/573106919975'
-
-      })
+        window.open(
+          'https://wa.me/573001234567',
+          '_blank'
+        )
+      }
+    )
   },
 })
